81778d99f22925f5b9ccffca6980dd95e3a9c47a
Ueder Cardoso <ueder.mds.cardoso@gmail.com>
2025-01-03 13:00:28 -0300

    save

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,25 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.jetbrains.kotlin) apply false
   alias(libs.plugins.jetbrains.dokka) apply true
+  `maven-publish`
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/uedermdscardoso/RootEncoder")
+      credentials {
+        username = System.getenv("GITHUB_USERNAME")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+    }
+  }
 }
 
 tasks.register("clean") {
